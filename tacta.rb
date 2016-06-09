e0a40fb017de9f1c7a5beef03b0c993c236b405d
Rosiene <rosiene.dilly@gmail.com>
2016-06-09 10:28:46 +0200

    Lood and quit

--- tacta.rb
+++ tacta.rb
@@ -25,11 +25,19 @@ contacts << { name: "Nikola Tesla"    , phone: "+385 43 987 3355", email: "nik@i
 contacts << { name: "Genghis Khan"    , phone: "+976 2 194 2222" , email: "contact@empire.com" }
 contacts << { name: "Malcom X"        , phone: "+1 310 155 8822" , email: "x@theroost.org"     }
 
-index(contacts)
+loop do
+  index(contacts)
 
-response = ask("Who would you like to see? ")
+  response = ask("Who would you like to see? (q to quit)")
+  break if response == "q"
 
-i = response.to_i
-contact = contacts[i-1]
+  i = response.to_i
+  contact = contacts[i-1]
 
-show(contact)
+  puts
+  show(contact)
+  puts
+end
+
+puts
+puts "Bye!"
